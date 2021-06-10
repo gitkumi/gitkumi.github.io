4eeb0792b6c956b3b177dd7f5b68075432819d2b
Takumi Ito <ito.tkmi@gmail.com>
2021-06-11 07:20:06 +0900

    Updates

--- _next/static/4G_9EHRENZLD4_2VTCxZx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-46670266fd93a0ee7127.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/BcZtY3jOd83955RFRO2HS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c33e16f29ff50de32c18.css","static/chunks/pages/index-35c3cf25fa96ceed409c.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
